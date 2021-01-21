5aa529f245d4a7fa683895a94016008fc5b9bef6
ASO1701016 <1701016@st.asojuku.ac.jp>
2021-01-21 19:38:49 +0900

    [FIX]エラー等を排除

--- src/components/CountTraining.vue
+++ src/components/CountTraining.vue
@@ -10,14 +10,9 @@
             </select>
 
             <div class="row mt-3 mb-5">
-                <button id="send_btn" class="btn btn-success" @click="new SendData(form.id)">決定</button>
+                <button id="send_btn" class="btn btn-success" @click="next()">決定</button>
             </div>
-
-            <!--            <button id="droplist_button" class="btn btn-success col-8 mx-auto" @click="dropList"></button>-->
-
             <div class="text-center mt-3" >
-                <!--              <button id="post_btn" class="btn btn-success col-8 mx-auto"  name="SetMenu" @click="NextPage">開始</button>-->
-                <!--              <router-link :to="{ name: 'formjudge', params: { met: 'met'}}" >開始</router-link >-->
             </div>
         </div>
     </div>
@@ -40,10 +35,9 @@
             }
         },
         methods:{
-            SendData(EventId){
-                let EventLink = {name:'formjudge',EventLink:EventId}
-                this.$router.push(EventLink).catch(() => {})
-            },
+          next(){
+            this.$router.push('/formjudge')
+          },
         },
     }
 </script>
--- src/components/FormJudge.vue
+++ src/components/FormJudge.vue
@@ -13,13 +13,12 @@
 
         <h3>選択トレーニング</h3>
         <div id="training">
-<!--          <Training v-on:select-category="this.selectCategory"/>-->
         </div>
 
         <div class="reset">
           <button id="btn_reset" @click="reset();">リセット</button>
           <div class="judge">
-            <button class="btn btn-outline-success" @click="this.Data_post">終了</button>
+            <button id="btn_comment" class="btn btn-outline-success" @click="comment();">終了</button>
           </div>
         </div>
         <canvas ref="canvas" id="canvas" width="500" height="500"></canvas>
@@ -32,6 +31,11 @@
 
 <script>
 export default {
+  // props:{
+  //   EventLink:{
+  //     type:String,
+  //   }
+  // }
 }
 </script>
 <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/posenet"></script>
@@ -58,6 +62,7 @@ export default {
       should_count: true,
       count_disp: document.getElementById("dips_count"),
       reset_btn: document.getElementById("btn_reset"),
+      comment_btn: document.getElementById("btn_comment"),
       keypoint: '',
       canvas: {},
     }
@@ -74,7 +79,6 @@ export default {
     }
 
     //poseNetのセットアップ処理
-    console.log("挙動確認")
     // this.video = p5.createCapture();
     this.posenet = ml5.poseNet(this.video,this.modelReady());
     let val=[];
@@ -101,28 +105,22 @@ export default {
     //回数をカウントするメソッド
     count: function () {
       this.count_value += 1;
-      console.log(this.count_value);
       this.count_disp.innerHTML = this.count_value;
     },
 
     draw: function (poses) {
       this.poses = poses;
-      console.log('描画を開始')
       if (this.poses.length > 0) {
         let pose = this.poses[0].pose;
         this.keypoint = pose.keypoints[0];
         for (let i = 0; i < this.poses.length; i++) {
           // poseが持つ情報を出力
-          console.log('poseが持つ情報を出力します')
           let pose = this.poses[i].pose
           //本当は0.6以上だが挙動確認のため低い値に設定
           if (pose.score >= 0.3) {
-            console.log("ok");
             //ユーザーが判定ラインより下にいった時
             //本当は250以上だが挙動確認のため低い値に設定
             if (pose.nose.y >= 220.0 && this.should_count) {
-              console.log('カウントします')
-              console.log(pose.nose.y);
               this.count_value += 1;
               this.should_count = false;
             } else if (pose.nose.y <= 240.0) {
@@ -134,7 +132,6 @@ export default {
             }
           }
         }
-        this.drawSkeleton();
         this.drawKeypoints();
       }
       else {
@@ -142,8 +139,9 @@ export default {
       }
     },
 
+
+
     drawKeypoints: function () {
-      console.log('drawKeypoints起動')
       for (let i = 0; i < this.poses.length; i++) {
         let pose = this.poses[i].pose;
         for (let j = 0; j < pose.length; j++) {
@@ -159,60 +157,14 @@ export default {
       }
     },
 
-    drawSkeleton: function () {
-      console.log('drawSkeleton起動')
-      for (let i = 0; i < this.poses.length; i++) {
-        let skeleton = this.poses[i].skeleton;
-        for (let j = 0; j < skeleton.length; j++) {
-          let partA = skeleton[j][0];
-          let partB = skeleton[j][1];
-
-          //骨格の線を引く
-          this.canvas = this.$refs.canvas
-          let ctx = this.canvas.getContext("2d");
-          ctx.strokeStyle = "green";
-        }
-      }
-    },
-
     //回数の値をリセットするメソッド
     reset: function () {
       console.log("リセットしました")
       this.count_value = 0;
     },
-    //-------------------------データ保存処理--------------------------------------
-    Data_post:async function (array) {
-      const URL = "https://fat3lak1i2.execute-api.us-east-1.amazonaws.com/acsys/users/schedule/motion/judge"
-      this.post_data = {
-        // judgeRate: Number(this.$store.state.judge_rate), //判定結果（カウント値）
-        add_date:Number(this.$store.state.add_date),
-        motion_calorie: this.$store.state.motion_calorie,
-        motion_name: this.$store.state.motion_name,
-      }
-
-      const json_data = JSON.stringify(this.post_data)
-      await fetch(URL, {
-        mode: 'cors',
-        method: 'POST',
-        body: json_data,
-        headers: {'Content-type': 'application'},
-      })
-          .then(function (response) {
-            return response.json()
-          })
-          .then(data => {
-            const flg_data = data['isSuccess']
-            if(flg_data){
-              this.$store.commit('judgeADD',check)
-              this.$router.replace("/savecalorie")
-            }else {
-              console.log('form_judge:NG')
-            }
-          })
-          .catch(function (error) {
-            console.log(error)
-          })
-    }
+    comment: function(){
+      alert('今回の回数は'+this.count_value+'でした')
+    },
   }
 }
 
